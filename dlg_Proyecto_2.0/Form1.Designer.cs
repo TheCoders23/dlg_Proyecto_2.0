ad7a2332fc41c463f248ad810477b116015da061
alexi <alexi@ALEXIS>
2023-09-09 15:17:37 -0600

    Agregar archivos de proyecto.

---
+++ dlg_Proyecto_2.0/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace dlg_Proyecto_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbservidor = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbcontrasena = new System.Windows.Forms.TextBox();
+            this.tbusuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbservidor
+            // 
+            this.tbservidor.Location = new System.Drawing.Point(91, 23);
+            this.tbservidor.Name = "tbservidor";
+            this.tbservidor.Size = new System.Drawing.Size(142, 20);
+            this.tbservidor.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tbcontrasena
+            // 
+            this.tbcontrasena.Location = new System.Drawing.Point(91, 101);
+            this.tbcontrasena.Name = "tbcontrasena";
+            this.tbcontrasena.Size = new System.Drawing.Size(142, 20);
+            this.tbcontrasena.TabIndex = 12;
+            // 
+            // tbusuario
+            // 
+            this.tbusuario.Location = new System.Drawing.Point(91, 58);
+            this.tbusuario.Name = "tbusuario";
+            this.tbusuario.Size = new System.Drawing.Size(142, 20);
+            this.tbusuario.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Contrasena";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Servidor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 199);
+            this.Controls.Add(this.tbservidor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbcontrasena);
+            this.Controls.Add(this.tbusuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbservidor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbcontrasena;
+        private System.Windows.Forms.TextBox tbusuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
